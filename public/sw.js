6c582e19b489340bfe13ec1e2cf7cd5e46ff2b67
Administrator <admin@news-collector.local>
2026-07-23 20:36:08 +0800

    switch SW to network-first strategy

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,7 @@
-const CACHE='news-v4';
-const ASSETS=['/','/css/style.css','/js/app.js?v=3','/manifest.json'];
-
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
-
+const CACHE='news-v5';
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))));e.waitUntil(self.clients.claim())});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-    if(res.ok){const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone))}
-    return res;
-  }).catch(()=>caches.match('/')))
-  );
+  e.respondWith(fetch(e.request).then(res=>{const r=res.clone();caches.open(CACHE).then(c=>c.put(e.request,r));return res}).catch(()=>caches.match(e.request)))
 });
